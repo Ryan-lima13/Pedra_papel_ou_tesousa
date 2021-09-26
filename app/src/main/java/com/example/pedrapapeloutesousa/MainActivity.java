a1c7e8a3b225e4a30e4be8a131e508d2ad35b75f
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-09-25 22:07:56 -0300

    adicionando metodos

--- app/src/main/java/com/example/pedrapapeloutesousa/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeloutesousa/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pedrapapeloutesousa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view){
+
+        this.opcaoSelecionada("pedra");
+
+    }
+    public void selecionadoTesoura(View view){
+
+        this.opcaoSelecionada("tesoura");
+
+    }
+    public void selecionadoPapel(View view){
+
+        this.opcaoSelecionada("papel");
+
+    }
+
+
+    public  void opcaoSelecionada(String opcaoSelecionada){
+        System.out.println(opcaoSelecionada);
+
     }
 }
